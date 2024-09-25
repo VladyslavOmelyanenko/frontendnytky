e4942a1b8cf54ae608d23f4458a29535ab0d6665
VladyslavOmelyanenko <36175376+VladyslavOmelyanenko@users.noreply.github.com>
2024-09-25 22:31:06 +0200

    Update website

--- src/pages/EmbroideryCirclesPage/EmbroideryCirclesPage.js
+++ src/pages/EmbroideryCirclesPage/EmbroideryCirclesPage.js
@@ -1,5 +1,7 @@
 import { PortableText } from "@portabletext/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useTranslation } from "react-i18next";
+
 
 import NavigationMenu from "../../components/NavigationMenu/NavigationMenu";
 import MobileNavigation from "../../components/MobileNavigation/MobileNavigation";
@@ -11,7 +13,11 @@ import useFetchData from "../../hooks/useFetchData";
 import styles from "./EmbroideryCirclesPage.module.scss";
 
 const EmbroideryCirclesPage = () => {
+  const { t } = useTranslation();
   const language = Language();
+  const scrollContainerRef = useRef(null);
+  const returnButtonRef = useRef(null);
+
 
   const GROQ_QUERY =
     language &&
@@ -23,20 +29,54 @@ const EmbroideryCirclesPage = () => {
   data && console.log(data);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
 
+  const scrollBack = () => {
+    const container = scrollContainerRef.current;
+    container.scrollTo({
+      left: 0,
+      behavior: "smooth",
+    });
+    returnButtonRef.current.style.display = "none";
+  };
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 800);
     };
     handleResize();
     window.addEventListener("resize", handleResize);
+    const handleWheel = (event) => {
+      if (!isMobile) {
+        event.preventDefault();
+         if (scrollContainerRef.current) {
+           const container = scrollContainerRef.current;
+           if (container.scrollLeft > 360) {
+             returnButtonRef.current.style.display = "flex";
+           } else {
+             returnButtonRef.current.style.display = "none";
+           }
+
+           event.preventDefault();
+           container.scrollLeft += event.deltaY;
+         }
+      }
+    };
+
+    const scrollContainer = scrollContainerRef.current;
+
+    if (scrollContainer) {
+      scrollContainer.addEventListener("wheel", handleWheel);
+    }
 
     return () => {
       window.removeEventListener("resize", handleResize);
+      if (scrollContainer) {
+        scrollContainer.removeEventListener("wheel", handleWheel);
+      }
     };
   });
 
   return (
-    <main>
+    <main className={styles.main} ref={scrollContainerRef}>
       <section className={styles.navigationSection}>
         {isMobile ? <MobileNavigation /> : <NavigationMenu fz={"40px"} />}
       </section>
@@ -89,6 +129,25 @@ const EmbroideryCirclesPage = () => {
       {!isMobile && (
         <div className={styles.languageMenu}>
           <LanguageMenu />
+          <div
+            className={styles.returnButton}
+            ref={returnButtonRef}
+            onClick={() => scrollBack()}
+          >
+            <svg
+              id="b"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 131.91 24.24"
+            >
+              <g id="c">
+                <path d="m25.48,23.13c-.1.22-.24.42-.43.6-.19.18-.41.3-.65.38-.24.08-.49.12-.74.12s-.5-.06-.72-.19L1.1,13.82c-.51-.22-.85-.61-1.01-1.15-.16-.54-.11-1.06.14-1.54.22-.35.51-.61.86-.77L22.94.19c.48-.22.97-.25,1.46-.07.5.18.86.5,1.08.98.22.48.25.97.07,1.46-.18.5-.5.86-.98,1.08L6.48,12.09l18.1,8.5c.32.13.58.34.77.62s.3.6.34.94c.03.34-.03.66-.19.98Z" />
+                <path d="m61.28,15.33c-.02.35-.13.67-.32.94-.19.28-.44.5-.73.65-.3.16-.62.23-.97.21l-14.43-.79-14.47-.89c-.51-.03-.95-.25-1.31-.65s-.53-.86-.5-1.38c.02-.35.13-.67.32-.94.19-.28.44-.5.73-.65.3-.16.62-.23.97-.21l14.47.89,14.43.79c.51.03.95.25,1.31.65.36.41.53.86.5,1.37Z" />
+                <path d="m97.51,10.5c.08.34.07.67-.03,1-.1.32-.27.6-.51.84-.24.24-.53.4-.87.48l-14.03,3.47-14.1,3.39c-.5.12-.98.04-1.44-.24s-.76-.67-.88-1.17c-.08-.34-.07-.67.03-1,.1-.32.27-.6.51-.84.24-.24.53-.4.87-.48l14.1-3.39,14.03-3.47c.5-.12.98-.04,1.44.24.46.28.76.67.88,1.17Z" />
+                <path d="m131.69,22.19c-.17.31-.4.55-.69.72-.29.17-.61.26-.94.28-.34.02-.66-.06-.97-.23l-12.73-6.84-12.72-6.95c-.45-.24-.75-.63-.91-1.15s-.11-1.01.13-1.46c.17-.31.4-.55.69-.72.29-.17.61-.26.94-.28.34-.02.66.06.97.22l12.72,6.95,12.73,6.84c.45.24.75.63.91,1.15.16.52.11,1.01-.13,1.46Z" />
+              </g>
+            </svg>
+            {t("Return")}
+          </div>
         </div>
       )}
     </main>
--- src/pages/EventsPage/EventsPage.js
+++ src/pages/EventsPage/EventsPage.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
+
 
 
 import NavigationMenu from '../../components/NavigationMenu/NavigationMenu';
@@ -10,33 +11,37 @@ import Language from "../../hooks/Language";
 import useFetchData from "../../hooks/useFetchData";
 
 
-
-import img1 from './1.png'
-import img2 from './2.png'
-import img3 from './3.png'
-import img4 from './4.png'
-
-
 import styles from './EventsPage.module.scss'
 
 const EventsPage = () => {
   
   const language = Language();
   const { t } = useTranslation();
+  const scrollContainerRef = useRef(null);
+  const returnButtonRef = useRef(null);
 
-  const GROQ_QUERY = language && `*[_type=="events" && language== "${language}"]{
-    EventsDates[]{
-      date,
-      times,
-      place
-    } 
-  }`;
+
+
+ const GROQ_QUERY = language && `*[_type == "events" && language == "${language}"]{
+  EventsDates[]{
+    date,
+    times,
+    place,
+    "images": eventImagesRef->eventImages[]{ 'imageUrl': asset->url }
+  }
+}`;
+
 
   const events = useFetchData(GROQ_QUERY);
-  events && console.log(events);
+  console.log(events);
 
   const pastEvents = [];
   const futureEvents = [];
+  const eventsWithPhotos = events && events.EventsDates.filter((event) => {
+    return (!!event.images)
+  })
+
+  console.log(eventsWithPhotos)
 
   events && events.EventsDates.forEach(event => {
     const currentDate = new Date();
@@ -65,8 +70,16 @@ const EventsPage = () => {
 
 
   
-  // const pastEvents = events.filter(event => event.date)
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+
+   const scrollBack = () => {
+     const container = scrollContainerRef.current;
+     container.scrollTo({
+       left: 0,
+       behavior: "smooth",
+     });
+     returnButtonRef.current.style.display = "none";
+   };
 
   useEffect(() => {
     const handleResize = () => {
@@ -75,14 +88,40 @@ const EventsPage = () => {
     handleResize();
     window.addEventListener("resize", handleResize);
 
+    const handleWheel = (event) => {
+      if (!isMobile) {
+        event.preventDefault();
+       if (scrollContainerRef.current) {
+         const container = scrollContainerRef.current;
+         if (container.scrollLeft > 360) {
+           returnButtonRef.current.style.display = "flex";
+         } else {
+           returnButtonRef.current.style.display = "none";
+         }
+
+         event.preventDefault();
+         container.scrollLeft += event.deltaY;
+       }
+      }
+    };
+
+    const scrollContainer = scrollContainerRef.current;
+
+    if (scrollContainer) {
+      scrollContainer.addEventListener("wheel", handleWheel);
+    }
+
     return () => {
       window.removeEventListener("resize", handleResize);
+      if (scrollContainer) {
+        scrollContainer.removeEventListener("wheel", handleWheel);
+      }
     };
   });
 
 
   return (
-    <main className={styles.main}>
+    <main className={styles.main} ref={scrollContainerRef}>
       <section className={styles.navigationSection}>
         {isMobile ? <MobileNavigation /> : <NavigationMenu fz={"40px"} />}
       </section>
@@ -126,29 +165,52 @@ const EventsPage = () => {
           </ul>
         </section>
         <section className={styles.archive}>
-          <h3>
-            January 20 <br></br> Botel, Amsterdam
-          </h3>
-          <div className={styles.images}>
-            <img src={img1} alt=""></img>
-            <img src={img2} alt=""></img>
-            <img src={img3} alt=""></img>
-            <img src={img4} alt=""></img>
-          </div>
-          <h3>
-            January 20 <br></br> Botel, Amsterdam
-          </h3>
-          <div className={styles.images}>
-            <img src={img1} alt=""></img>
-            <img src={img2} alt=""></img>
-            <img src={img3} alt=""></img>
-            <img src={img4} alt=""></img>
-          </div>
+          {eventsWithPhotos &&
+            eventsWithPhotos.map((event) => {
+              return (
+                <>
+                  <h3>
+                    {new Date(event.date).getDate() +
+                      " " +
+                      t(monthsNames[new Date(event.date).getMonth()])}
+                    <br></br>
+                    {event.place}
+                  </h3>
+                  <div className={styles.images}>
+                    {event.images.map((image, i) => (
+                      <img
+                        src={image.imageUrl}
+                        alt={event.date + "image" + i}
+                      ></img>
+                    ))}
+                  </div>
+                </>
+              );
+            })}
         </section>
       </section>
       {!isMobile && (
         <div className={styles.languageMenu}>
           <LanguageMenu />
+          <div
+            className={styles.returnButton}
+            ref={returnButtonRef}
+            onClick={() => scrollBack()}
+          >
+            <svg
+              id="b"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 131.91 24.24"
+            >
+              <g id="c">
+                <path d="m25.48,23.13c-.1.22-.24.42-.43.6-.19.18-.41.3-.65.38-.24.08-.49.12-.74.12s-.5-.06-.72-.19L1.1,13.82c-.51-.22-.85-.61-1.01-1.15-.16-.54-.11-1.06.14-1.54.22-.35.51-.61.86-.77L22.94.19c.48-.22.97-.25,1.46-.07.5.18.86.5,1.08.98.22.48.25.97.07,1.46-.18.5-.5.86-.98,1.08L6.48,12.09l18.1,8.5c.32.13.58.34.77.62s.3.6.34.94c.03.34-.03.66-.19.98Z" />
+                <path d="m61.28,15.33c-.02.35-.13.67-.32.94-.19.28-.44.5-.73.65-.3.16-.62.23-.97.21l-14.43-.79-14.47-.89c-.51-.03-.95-.25-1.31-.65s-.53-.86-.5-1.38c.02-.35.13-.67.32-.94.19-.28.44-.5.73-.65.3-.16.62-.23.97-.21l14.47.89,14.43.79c.51.03.95.25,1.31.65.36.41.53.86.5,1.37Z" />
+                <path d="m97.51,10.5c.08.34.07.67-.03,1-.1.32-.27.6-.51.84-.24.24-.53.4-.87.48l-14.03,3.47-14.1,3.39c-.5.12-.98.04-1.44-.24s-.76-.67-.88-1.17c-.08-.34-.07-.67.03-1,.1-.32.27-.6.51-.84.24-.24.53-.4.87-.48l14.1-3.39,14.03-3.47c.5-.12.98-.04,1.44.24.46.28.76.67.88,1.17Z" />
+                <path d="m131.69,22.19c-.17.31-.4.55-.69.72-.29.17-.61.26-.94.28-.34.02-.66-.06-.97-.23l-12.73-6.84-12.72-6.95c-.45-.24-.75-.63-.91-1.15s-.11-1.01.13-1.46c.17-.31.4-.55.69-.72.29-.17.61-.26.94-.28.34-.02.66.06.97.22l12.72,6.95,12.73,6.84c.45.24.75.63.91,1.15.16.52.11,1.01-.13,1.46Z" />
+              </g>
+            </svg>
+            {t("Return")}
+          </div>
         </div>
       )}
     </main>
